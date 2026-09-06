7d0fd64d2f7fc0f2eb21428a22266c4350b36497
Akshat___567 <123239205+akshat23a@users.noreply.github.com>
2026-09-06 15:28:59 +0530

    Time: 0 ms (100.00%) | Memory: 43.8 MB (47.45%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.java
+++ 88-merge-sorted-array/merge-sorted-array.java
@@ -1,44 +1,40 @@
 class Solution {
-
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-
-        int i = 0;
-        int j = 0;
-        int id = 0;
-
-        int[] res = new int[m + n];
-
-        while (i < m && j < n) {
-
-            if (nums1[i] <= nums2[j]) {
-                res[id] = nums1[i];
+        int i=0;
+        int j=0;
+        int id=0;
+        int res[]=new int[m+n];
+        while(i<m&j<n){
+            if(nums1[i]<nums2[j]){
+                res[id]=nums1[i];
                 i++;
-            } 
-            else {
-                res[id] = nums2[j];
+                id++;
+            }else if(nums1[i]==nums2[j]){
+                res[id]=nums1[i];
+                id++;
+                i++;
+                res[id]=nums2[j];
+                id++;
                 j++;
             }
-
-            id++;
+            else {
+                  res[id] = nums2[j];
+                     id++;
+                     j++;
+}
         }
-
-        // Remaining elements of nums1
-        while (i < m) {
-            res[id] = nums1[i];
-            i++;
-            id++;
-        }
-
-        // Remaining elements of nums2
-        while (j < n) {
-            res[id] = nums2[j];
-            j++;
-            id++;
-        }
-
-        // Copy result into nums1
-        for (int k = 0; k < m + n; k++) {
-            nums1[k] = res[k];
+            while(j<n){
+                res[id]=nums2[j];
+                id++;
+                j++;
+            }
+            while(i<m){
+                res[id]=nums1[i];
+                id++;
+                i++;
+            }
+            for(int k=0;k<m+n;k++){
+                nums1[k]=res[k];
+            }
         }
     }
-}
